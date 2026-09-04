356a896dfe62db3ec6d624b002adfb987f5939e4
Jason <jason_morgan84@yahoo.co.uk>
2026-09-04 17:43:55 +0100

    feat: added __call__ for enum

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -6,3 +6,20 @@ class DataType(Enum):
     ImageBinary = auto()       
     ValueInt = auto()      
     ValueFloat = auto()  
+
+    def __call__(self, *args, **kwargs):
+        from core.type import ImageInt, ImageFloat, ImageBinary, ValueInt, ValueFloat
+        
+        mapping = {
+            DataType.ImageInt: ImageInt,
+            DataType.ImageFloat: ImageFloat,
+            DataType.ImageBinary: ImageBinary,
+            DataType.ValueInt: ValueInt,
+            DataType.ValueFloat: ValueFloat,
+        }
+        
+        target_class = mapping.get(self)
+        if target_class is None:
+            raise NotImplementedError(f"No class mapped for data type: {self.name}")
+            
+        return target_class(*args, **kwargs)
