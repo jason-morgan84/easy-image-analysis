ce28c9fefae9bc4fcdcefb29fd70005dab79d2cb
Jason <jason_morgan84@yahoo.co.uk>
2026-09-05 11:05:25 +0100

    feat: updated DataType in constants.py to categorise types into image_types and value_types

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -7,6 +7,10 @@ class DataType(Enum):
     ValueInt = auto()      
     ValueFloat = auto()  
 
+    #categorise DataTypes into image_types, for multi-dimensional arrays, and value_types, for 1d variables.
+    image_types = {ImageInt, ImageFloat, ImageBinary}
+    value_types = {ValueInt, ValueFloat}
+
     def __call__(self, *args, **kwargs):
         from core.type import ImageInt, ImageFloat, ImageBinary, ValueInt, ValueFloat
         
