4372e00475bc2c689614843a0a77a4914194e18b
Jason <jason_morgan84@yahoo.co.uk>
2026-09-12 15:18:27 +0100

    feat: updated DataType with __getattr__ to get attributes of member classes

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -22,6 +22,13 @@ class DataType(Enum):
     def array_types(cls) -> set:
         return {cls.ArrayInt, cls.ArrayFloat}
 
+    @classmethod
+    def types(cls) -> set:
+        return {cls.ImageInt, cls.ImageFloat, cls.ImageBinary,
+                cls.ValueInt, cls.ValueFloat,
+                cls.ArrayInt, cls.ArrayFloat}
+                
+
     # allows resolving of target class from DataType (e.g, can access DataType.ValueInt.to_numpy() as DataType.ValueInt.target_class.to_numpy()
     @property
     def target_class(self):
@@ -42,10 +49,21 @@ class DataType(Enum):
             raise NotImplementedError(f"No class mapped for data type: {self.name}")
         return cls_
 
-    # gets to numpy class variable using DataType.Type.numpy rather than DataType.Type.target_class.numpy
-    @property
-    def numpy(self):
-        return self.target_class.numpy
+    # Automatically gets attribute lookups from the underly enum target class
+    # ie, can do DataType.ImageInt.numpy rather than DataType.Type.target_class.numpy
+    def __getattr__(self, name):
+        
+       # Prevents infinite recursion during Enum initialization
+        if name.startswith("_"):
+            raise AttributeError(name)
+
+        # returns attribute if it exists
+        target = self.target_class
+        if hasattr(target, name):
+            return getattr(target, name)
+
+        # otherwise, returns error
+        raise AttributeError(f"target class {target.__name__} has no attribute {name}")
 
     # allows direct instantiation of target_class (ie, can say a = DataType.ValueInt(5) and a will be a member of ValueInt with value = 5)
     def __call__(self, *args, **kwargs):
