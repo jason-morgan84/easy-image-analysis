86bf212798f6132e58529bb011ea589d3e0706fb
Jason <jason_morgan84@yahoo.co.uk>
2026-09-13 15:35:55 +0100

    refactor: Updated Image class to updated class heirarchy

--- backend/core/image.py
+++ backend/core/image.py
@@ -3,79 +3,49 @@ from core.constants import DataType
 from core.shape import Shape
 
 class Image:
-    def __init__(self, array, array_dtype, image_shape, image_mapping):
-        self.array = array                      # the multi-dimensional array that holds the pixel data
-        self.array_dtype = array_dtype          # the DataType used to store the pixel data
-        self.image_shape = image_shape          # the shape of the *image* - note: this is distinct from the size of the array
-        self.image_mapping = image_mapping      # mapping of image dimensions (c,z,y,x) to Array dimensions (0,1,2,3 etc)
+    def __init__(self, pixel_array, image_map):
+        self.pixel_array = pixel_array                      # the multi-dimensional array that holds the pixel data
+        self.image_map = image_map                          # mapping of image dimensions (c,z,y,x) to Array dimensions (0,1,2,3 etc)
 
     @property
-    def array(self):
-        return self._array
+    def pixel_array(self):
+        return self._pixel_array
 
-    @array.setter
-    def array(self, arr):
+    @pixel_array.setter
+    def pixel_array(self, arr):
         # check array is of one of the acceptable data types defined by DataType.image_types() in constants.py
         array_dtype = getattr(arr, "data_type", None)
 
         if array_dtype not in DataType.image_types() or array_dtype == None:
             raise TypeError (f"Expected Image data type (see constants.py) got {type(arr)}")
 
-        # check array has 4 dimensions
+        # check array has correct number of dimensions
         array_size = len(arr.value.shape)
-        if array_size != 4:
-            raise ValueError (f"array should have 4 dimensions (c,z,y,x) but has {array_size}")
+        if array_size > Shape.max_image_dimensions or array_size < Shape.min_image_dimensions:
+            raise ValueError (f"array should have {Shape.min_image_dimensions}-{Shape.max_image_dimensions} dimensions but has {array_size}")
 
-        self._array = arr
-
-    @property
-    def array_dtype(self):
-        return self._array_dtype
-
-    @array_dtype.setter
-    def array_dtype(self, dtype):
-        # check defined array_dtype matches actual array data type
-        if dtype != self.array.data_type:
-            raise TypeError (f"array_dtype {dtype} does not match array data type {type(self.array)}")
-
-        self._array_dtype = dtype
-
-    @property 
-    def image_shape(self):
-        return self._image_shape
-
-    @image_shape.setter
-    def image_shape(self, shape):
-        ndim = 0
-        # checks the number of defined image dimensions in image_shape matches the actual size of the array
-        ndim += sum(1 for item in shape if item > 0)
-        array_size = len(self.array.value.shape)
-
-        # give ValueError if they don't match
-        if (ndim != array_size):
-            raise ValueError (f"image_shape {shape} defines {ndim} dimensions but array contains {array_size}")
-
-        self._image_shape = shape
+        self._pixel_array = arr
 
     @property
-    def image_mapping(self):
-        return self._image_mapping
+    def image_map(self):
+        return self._image_map
 
-    @image_mapping.setter
-    def image_mapping(self, map):
-        # for each dimension, check that if, for example, image shape says channel c has size 4 and image mapping says channel c maps to array dimension 0,
-        # array dimension 0 also has size 4, else return ValueError
-        
-        array_shape = self.array.value.shape
-        for dimension in Shape.dimensions:
-            dimension_array_shape = array_shape[getattr(map, dimension)]
-            dimension_image_shape = getattr(self.image_shape,dimension)
-            if dimension_array_shape != dimension_image_shape:
-                raise ValueError (f"{dimension} dimension of size {dimension_image_shape} is mapped to array dimension of size {dimension_array_shape}")
+    @image_map.setter
+    def image_map(self, map):
+        if not isinstance(map, Shape):
+            raise TypeError (f"Expected image_map of Shape class, got {type(map)}")
+        # check image_map is a Shape class
 
-        self._image_mapping = map
+        self._image_map = map
 
-    def transpose(self, new_shape):
+    def get_image_shape(self):
+        return Shape(c = self.pixel_array.value.shape[self.image_map.c],
+                     z = self.pixel_array.value.shape[self.image_map.z],
+                     y = self.pixel_array.value.shape[self.image_map.y],
+                     x = self.pixel_array.value.shape[self.image_map.x])
+
+    # tranpose to be moved to WorkFlow
+    """def transpose(self, new_shape):
 
         # expect a list/tuple with four, non-duplicate string elements which are members of Shape.dimension_order
         if not isinstance(new_shape, tuple) and not isinstance(new_shape,list):
@@ -122,7 +92,7 @@ class Image:
                      array_dtype = self.array_dtype,
                      image_shape = self.image_shape,
                      #image_shape = Shape(*transposed_array.shape),
-                     image_mapping = Shape(*new_map))
+                     image_mapping = Shape(*new_map))"""
 
 
 
